02b810877aff9bf92283efd5ce1b2d82ef267873
Sonnyburni <sonnyburniston@yahoo.co.uk>
2023-03-06 12:33:57 +0000

    initial commit

---
+++ server/src/services/mongo.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB Connection Ready!');
+  });
+  
+  mongoose.connection.on('error', (err) => {
+    console.error(err);
+  });
+
+async function mongoConnect(){
+    await mongoose.connect(MONGO_URL);
+};
+
+async function mongoDisonnect(){
+    await mongoose.disconnect(MONGO_URL);
+};
+
+module.exports = {
+    mongoConnect,
+    mongoDisonnect,
+}
